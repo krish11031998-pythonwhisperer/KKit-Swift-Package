1f180227c2918267c102d9ed8c419b41be627a49
Krishna Venkatramani <krish_venkat11@hotmail.com>
2024-12-21 23:51:14 +0400

    Height for the label

--- Sources/KKit/UIView/UILabel/UILabel+Extension.swift
+++ Sources/KKit/UIView/UILabel/UILabel+Extension.swift
@@ -10,15 +10,17 @@ import Combine
 
 public extension UILabel {
     
-    func setAttributedTextToFitVertically(text: RenderableText, width: CGFloat) {
+    @discardableResult
+    func setAttributedTextToFitVertically(text: RenderableText, width: CGFloat) -> CGFloat {
         guard let attributedText = text as? NSAttributedString else {
             text.render(target: self)
-            return
+            return 0
         }
         
         let height = attributedText.height(withWidth: width)
         attributedText.render(target: self)
         setHeight(height: height)
+        return height
     }
     
 }
