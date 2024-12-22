404cac8c318bf5aee462e391be1cd3dbd39e1087
Krishna Venkatramani <krish_venkat11@hotmail.com>
2024-12-22 10:48:16 +0400

    Adding Extra Height

--- Sources/KKit/UIView/UILabel/UILabel+Extension.swift
+++ Sources/KKit/UIView/UILabel/UILabel+Extension.swift
@@ -11,13 +11,13 @@ import Combine
 public extension UILabel {
     
     @discardableResult
-    func setAttributedTextToFitVertically(text: RenderableText, width: CGFloat) -> CGFloat {
+    func setAttributedTextToFitVertically(text: RenderableText, width: CGFloat, extraHeight: CGFloat = 1) -> CGFloat {
         guard let attributedText = text as? NSAttributedString else {
             text.render(target: self)
             return 0
         }
         
-        let height = attributedText.height(withWidth: width)
+        let height = attributedText.height(withWidth: width) + extraHeight
         attributedText.render(target: self)
         setHeight(height: height)
         return height
